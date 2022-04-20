b14698d9ac4037a4a8fb1fbebe73f95eb6200f1c
wg <wg@LAPTOP-93TBBP4S>
2022-04-20 11:43:23 +0800

    添加项目文件。

---
+++ student/Data/studentContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using student.Models;
+
+namespace student.Data
+{
+    public class studentContext : DbContext
+    {
+        public studentContext (DbContextOptions<studentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<student.Models.Student> Student { get; set; }
+    }
+}
---
+++ student/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace student.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Sex { get; set; }
+        public int Age { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+        public string Phone { get; set; }
+        public int Temperature { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+    }
+}
